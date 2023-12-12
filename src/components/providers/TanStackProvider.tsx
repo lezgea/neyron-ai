861f6a61f5f7d46b0508e2215b966dd9ebc999ca
Rahil <rahil.eliyev@gmail.com>
2023-12-12 17:48:03 +0400

    fix coming soon page

--- src/components/providers/TanStackProvider.tsx
+++ src/components/providers/TanStackProvider.tsx
@@ -1,8 +1,8 @@
-"use client";
+'use client';
 
-import React, { useState } from "react";
-import { QueryClient,QueryClientProvider } from "@tanstack/react-query";
-
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 interface Props {
   children: React.ReactNode;
@@ -10,7 +10,12 @@ interface Props {
 
 const TanStackProvider = ({ children }: Props) => {
   const [queryClient] = useState(() => new QueryClient());
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 };
 
 export default TanStackProvider;
