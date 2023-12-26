1fae6eeae489da3b118d9b1349db112d6168b843
Sahira23 <mammadovasahira@gmail.com>
2023-12-26 14:39:56 +0400

    removed content next config

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 /** @type {import('next').NextConfig} */
 // next.config.js
-const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(riv)$/,
-      use: {
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]',
-          publicPath: '/_next/static/files', // Adjust the path as needed
-          outputPath: 'static/files', // Adjust the path as needed
-        },
-      },
-    });
-    return config;
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
