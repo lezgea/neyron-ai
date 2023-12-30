95699fa363e908786d9b79e41294380077b90b83
Sahira23 <mammadovasahira@gmail.com>
2023-12-30 15:27:32 +0400

    accept language

--- src/components/home-page/containerPage.tsx
+++ src/components/home-page/containerPage.tsx
@@ -1,5 +1,12 @@
 'use client';
 
+import { useEffect } from 'react';
+import { Alignment, Fit, Layout, useRive } from '@rive-app/react-canvas';
+
+import { Grid } from '@mui/material';
+
+import LandingAnimation2 from '../../../public/landingAnimation2.riv';
+
 import AboutUs from './aboutUs';
 import BeginAdventure from './beginAdventure';
 import Community from './community';
@@ -9,6 +16,29 @@ import Feedbacks from './feedbacks';
 import WhyUs from './whyUs';
 
 const ContainerPage = () => {
+  const { rive, RiveComponent } = useRive(
+    {
+      src: LandingAnimation2,
+      autoplay: true,
+      stateMachines: 'State Machine 1',
+      layout: new Layout({
+        fit: Fit.Cover,
+        alignment: Alignment.Center,
+      }),
+    },
+    {
+      fitCanvasToArtboardHeight: true,
+    }
+  );
+  useEffect(() => {
+    if (rive) {
+      rive?.play('figures');
+      const eyeAnimation = () => {
+        rive?.play('eyes animation');
+      };
+      setTimeout(eyeAnimation, 3000);
+    }
+  }, [rive]);
   return (
     <div className="stack">
       <div className="stack__card">
@@ -18,19 +48,28 @@ const ContainerPage = () => {
         <BeginAdventure />
       </div>
       <div className="stack__card">
-        <AboutUs />
-      </div>
-      <div className="stack__card">
-        <WhyUs />
+        <Grid container className="container">
+          <Grid item xs={7} className="stacked_card_container">
+            {' '}
+            <div className="stacked_card">
+              {' '}
+              <AboutUs />
+              <WhyUs />
+            </div>
+          </Grid>
+          <Grid item xs={4}>
+            {RiveComponent && <RiveComponent className="rive-1" />}
+          </Grid>
+        </Grid>
       </div>
       <div className="stack__card">
         <Community />
       </div>
       <div className="stack__card">
-        <Faq mainPage={true} />
-      </div>
-      <div className="stack__card">
         <Feedbacks />
+      </div>
+      <div className="stack__card faq-stack">
+        <Faq mainPage={true} />
       </div>
     </div>
   );
