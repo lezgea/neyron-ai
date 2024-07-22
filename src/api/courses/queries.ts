25fa0e5a60f0e8851f68ea9a77097b0fba941d91
muslimragimov <muslim.ragimov@yahoo.com>
2024-07-21 23:37:36 -0400

    Chapters and courses API are connected

--- src/api/courses/queries.ts
+++ src/api/courses/queries.ts
@@ -5,9 +5,13 @@ import { courses, languages, v1 } from '../endpoints';
 import { QUERY_KEYS } from '../query_keys';
 
 
-export const useGetCourses = () => {
+export const useGetCourses = (lang: string) => {
     return useQuery([QUERY_KEYS.courses], async () => {
-        const { data } = await axiosInstance.get(v1 + courses + languages);
+        const { data } = await axiosInstance.get(v1 + courses + languages, {
+            headers: {
+                'Accept-Language': `${lang}`,
+            },
+        });
         return data;
     });
 };
