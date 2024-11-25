f91ac3bf97288dc794bfe0383f08ac46b090f569
lezgea <muslim.ragimov@yahoo.com>
2024-11-25 17:57:56 -0500

    About us page has been added

--- src/app/[lng]/about-us/page.tsx
+++ src/app/[lng]/about-us/page.tsx
@@ -20,16 +20,47 @@ const AboutUs: React.FC = () => {
             <div className="min-h-screen flex flex-col">
                 <a href="#main-content" className="sr-only focus:not-sr-only">Skip to main content</a>
                 <main id="main-content" className="container mx-auto flex-grow px-5 py-10 md:px-0 md:py-[6rem]">
-                    <h1 className="text-[32px] md:text-[2.3rem] font-medium">{t('aboutUs')}</h1>
                     {/* Breadcrumb */}
                     <nav className="text-sm flex justify-start items-center text-gray-600 space-x-3">
-                        <Link href="/" className="hover:text-primaryLight">{t('mainPage')}</Link>
+                        <Link href="/" className="hover:text-primaryLight">{t('main.mainPage')}</Link>
                         <span className="text-lg">&gt;</span>
-                        <span>{t('aboutUs')}</span>
+                        <span>{t('main.aboutHead')}</span>
                     </nav>
-                    <AboutSection description={t('aboutUsText1')} />
-                    <AboutSection description={t('aboutUsText2')} />
-                    <AboutSection description={t('aboutUsText3')} />
+                    <div className="ai-section flex flex-col items-center justify-center text-center px-40 pt-20 gap-10">
+                        <h1 className="ai-section__title ai-highlight text-4xl font-semi leading-[3rem] tracking-tighter md:text-6xl md:leading-[4.5rem]">
+                            Welcome to <span>Neyron.ai</span> - Your AI Learning Companion
+                        </h1>
+
+                        <div className="ai-highlight flex flex-col gap-5">
+                            <p>
+                                At <span>Neyron.ai</span>, our mission is to make Artificial Intelligence not just
+                                understandable but also enjoyable. Whether you're an AI enthusiast or a seasoned
+                                professional, we've created an environment where AI is accessible to everyone.
+                            </p>
+                            <div>
+                                <h4 className='text-xl font-semi mb-2'>Learning, Gamified:</h4>
+                                <p>
+                                    Say goodbye to the ordinary. Our gamified approach transforms education into an irresistibly
+                                    addictive adventure. Because learning should be as exciting as the subject itself, right?
+                                </p>
+                            </div>
+                            <div>
+                                <h4 className='text-xl font-semi mb-2'>Smart Learning, Simplified:</h4>
+                                <p>
+                                    Demystify AI effortlessly! Dive into interactive chapters, ace quizzes, and earn
+                                    points—ranking up your journey. With various ways to score, your AI adventure is not just
+                                    educational but also fun and rewarding!
+                                </p>
+                            </div>
+                            <div>
+                                <h4 className='text-xl font-semi mb-2'>Ready to Redefine Your AI Journey?</h4>
+                                <p>
+                                    Join <span>Neyron.ai</span> — where learning is not just friendly, engaging, and downright
+                                    awesome, but also designed for everyone. Let's embark on this AI adventure together!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
                 </main>
             </div>
         </Suspense>
