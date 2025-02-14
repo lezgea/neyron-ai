f130f140c641651b3ce4e10bb07c5cc0dfafe8e6
lezgea <muslim.ragimov@yahoo.com>
2025-02-14 00:24:05 -0500

    Courses banner has been added

---
+++ src/app/[lng]/ranking/page.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Link from 'next/link';
+import { Metadata } from 'next';
+import { useTranslations } from 'next-intl';
+import { CoursesTable } from '@components/features/courses';
+
+
+export const metadata: Metadata = {
+    title: "Ranking | Neyron AI",
+    description: "Neyron AI is an innovative platform designed to bring data scientists and AI enthusiasts together to compete in data-driven challenges.",
+};
+
+
+const Ranking: React.FC = () => {
+    const t = useTranslations();
+
+    return (
+        <div className="min-h-screen flex flex-col">
+            <a href="#main-content" className="sr-only focus:not-sr-only">Skip to main content</a>
+            <main id="main-content" className="container mx-auto flex-grow px-5 py-10 md:px-0 md:py-[6rem]">
+                {/* Breadcrumb */}
+                <nav className="text-sm flex justify-start items-center text-gray-600 space-x-3">
+                    <Link href="/" className="hover:text-purple">{t('main.mainPage')}</Link>
+                    <span className="text-lg">&gt;</span>
+                    <span>{t('footer.courses')}</span>
+                </nav>
+                {/* <section className="container mx-auto w-full">
+                    <div className="flex flex-col text-center justify-between items-center w-full">
+                        <h2 className="text-[32px] md:text-[2.3rem] font-medium">Introduction to AI</h2>
+                        <p className="text-md text-gray-700">This chapter gives you a basic overview of Artificial Intelligence, its key concepts, and technologies.</p>
+                    </div>
+                </section> */}
+                <section className="container mx-auto w-full mt-10">
+                    <CoursesTable />
+                </section>
+            </main>
+        </div>
+    );
+};
+
+export default Ranking;
